fccc61c5e2180148e85fb32c67f4ad472a4c95ce
Jp-Code-Repo <jpbuzz6100@gmail.com>
2026-07-03 11:49:29 +0800

    Fixed notif

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -2,23 +2,24 @@
 
 <script src="/assets/vendor/sweetalert2/sweetalert2.all.min.js"></script>
 
-    <?php if (!empty($_SESSION['toast'])): ?>
+
+    <?php if (flash_has('toast')): ?>
+
+    <?php $toast = flash('toast'); ?>
 
     <script>
 
     Swal.fire({
         toast: true,
         position: 'top-end',
-        icon: '<?= $_SESSION['toast']['type'] ?>',
-        title: <?= json_encode($_SESSION['toast']['message']) ?>,
+        icon: '<?= $toast['type'] ?>',
+        title: <?= json_encode($toast['message']) ?>,
         showConfirmButton: false,
         timer: 3000,
         timerProgressBar: true
     });
 
     </script>
-
-    <?php unset($_SESSION['toast']); ?>
 
     <?php endif; ?>
 
